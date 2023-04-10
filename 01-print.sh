8e08e23ace8657774863615a93b8507e2a3d9256
vamsi <vamsikrishna.chittam@gmail.com>
2023-04-10 15:15:31 +0530

    MySql script

---
+++ 01-print.sh
@@ -0,0 +1,10 @@
+echo Heloo
+
+# Color Syntax
+# echo -e "\e[COLmHello\e[0m"
+# -e - enable \e
+# \e - enable COL
+# COL - COL CODE , 31-26 (31- REd, 32 - Green, 33 - YELLOW, 34 - BLUE , 35 - MAGENTA, 36 - CYAN )
+# 0m - Disable the enabled color
+
+echo -e "\e[31mHello in RED Color\e[0m"
