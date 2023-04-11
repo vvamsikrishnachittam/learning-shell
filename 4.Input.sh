e061d925f7214e67c777bc8a551f3446b8d9380e
vamsi <vamsikrishna.chittam@gmail.com>
2023-04-11 17:08:38 +0530

    Leaning-shell

---
+++ 4.Input.sh
@@ -0,0 +1,8 @@
+#read -p 'Enter MySQL Root Username: ' user
+#read -s -p 'Enter MySQL Root Password: ' pass
+#
+#echo "Username & Password from read = $user & $pass"
+
+
+echo Username = $1
+echo Password = $2
